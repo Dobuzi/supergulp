860ae49efc4886028d6ae625d76e36afa1be920b
Dobuzi <kimjw7981@gmail.com>
2020-07-05 16:07:25 +0900

    pug task part one

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,14 @@
 import gulp from "gulp";
+import pug from "gulp-pug";
+
+const routes = {
+    pug: {
+        src: "src/*.pug",
+        dest: "build",
+    },
+};
+
+export const makeHTMLfromPug = () =>
+    gulp.src(routes.pug.src).pipe(pug()).pipe(gulp.dest(routes.pug.dest));
+
+export const dev = gulp.series([makeHTMLfromPug]);
